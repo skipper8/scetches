356ddfd19290bb2737a741ae684bc17511d4093d
skipper8 <mharper48@gmail.com>
2021-03-31 08:04:07 -0700

    enviro

---
+++ eviro_impact.py
@@ -0,0 +1,39 @@
+#_______________________________EVIRO_IMPACT___________________________________
+#
+#_________________________________ANALYSIS_____________________________________
+#GOAL: To have an easy to use app that allows people to track their enviormental foot print by imputting what they do and scanning in items
+#______________________________________________________________________________
+#Requirements:  compatability with calendar
+#		ability to learn habbits
+#		take user input of actvity and purcheses
+#		Caculate waste
+#		offer alternatives
+#______________________________Object_Model____________________________________
+#             |Objects      | Instances
+#_____________|_____________|_____________________________
+#     NAME    |PURCHESES    | an item bought
+#_____________|_____________|_____________________________
+#             |cost         |
+#             |attainment CF|
+# Attributes  |proccesing CF|
+#             |use          |
+#             |likeability  |
+#_____________|_____________|_____________________________
+#             |scan recpeit |JPG ==> DB entry
+#             |find in db   |
+# Opperations |find on web  |
+#             |calculate use|
+#             |calc like    | 
+#_____________|_____________|____________________________
+#KNOWN ITEMS  | items the app knows about
+#cost         |
+#how far      |
+#processing CF|
+#use          |
+#like ability |
+#HABIT        | what the user does normally
+#when         |
+#frequency    |
+#item used    |
+#quanity      |
+#why          |
