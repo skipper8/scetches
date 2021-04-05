9663e8e0bf6c75041206624081b048ddb686aa82
skipper8 <mharper48@gmail.com>
2021-04-05 08:40:47 -0700

    template and weight loss

---
+++ weight_loss_app.py
@@ -0,0 +1,45 @@
+#_____________________________WEIGHT_LOSS_APP_______________________________
+#GOAL: Help people loss weight in a healthy way and suggest next steps when desired based on current science and available information and estimated succsess rate
+#_________________________________Analysis____________________________________
+#REQUIREMENTS:  Keep track of diet and guess when person does not input information
+		Inform client of habits
+		Keep up to date with latest nutrition and athletic science
+		Make sugestions based on enjoyment, science, succsess rate
+#_______________________________Object_Model_________________________________
+#		|object		|instances
+#_______________|_______________|________________
+#FEATURE        |FOOD		|ROLL
+#		|		|spegetti
+#		|		|...		
+#_______________|_______________|________________
+#ATTRIBUTES     |calories       |
+#		|cost           |
+#		|nutrition      |
+#               |likeability    |               
+#_______________|_______________|________________
+#OPPERATIONS    |               |               
+#_______________|_______________|________________
+#FEATURE        |               |
+#_______________|_______________|________________
+#ATTRIBUTES     |               |               
+#_______________|_______________|________________
+#OPPERATIONS    |               |                
+#_______________|_______________|________________
+#FEATURE        |               |
+#_______________|_______________|________________
+#ATTRIBUTES     |               |               
+#_______________|_______________|________________
+#OPPERATIONS    |               |                
+#_______________|_______________|________________
+#FEATURE        |               |
+#_______________|_______________|________________
+#ATTRIBUTES     |               |               
+#_______________|_______________|________________
+#OPPERATIONS    |               |                
+#_______________|_______________|________________
+#FEATURE        |               |
+#_______________|_______________|________________
+#ATTRIBUTES     |               |               
+#_______________|_______________|________________
+#OPPERATIONS    |               |                
+#_______________|_______________|________________
